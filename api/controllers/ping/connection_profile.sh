50cb94cfd8178e897e2df74fa8af813fddf57a2f
Reekaa <forgacsreka88@gmail.com>
2023-06-18 14:07:46 +0100

    update createConnectionProfile

---
+++ api/controllers/ping/connection_profile.sh
@@ -0,0 +1,5 @@
+'curl --header \
+ "Authorization: Bearer $(gcloud auth application-default print-access-token)" \ --header \
+ "Content-Type: application/json" \ --data \
+ @./connection_profile.json \ -X \
+ POST \ https://datamigration.googleapis.com/v1/projects/db-migration-project-s2042203/locations/europe-west2/connectionProfiles?connectionProfileId=on-prem-sql-profile'
--- api/controllers/ping/handler.js
+++ api/controllers/ping/handler.js
@@ -1,24 +1,16 @@
-const util = require('util');
-const exec = require('child_process').exec;
+const shell = require('shelljs')
 
 const ping = async (req, res) => {
     res.apiResponse('pong')
 }
 
 const createConnectionProfile = async (req, res) => {
-    // res.apiResponse('bob')
-    const command = 'curl --header "Authorization: Bearer $(gcloud auth application-default print-access-token)" \ --header "Content-Type: application/json" \ --data @./connection_profile.json \ -X POST \ https://datamigration.googleapis.com/v1/projects/db-migration-project-s2042203/locations/europe-west2/connectionProfiles?connectionProfileId=on-prem-sql-profile'
-
-    child = exec(command, function (error, stdout, stderr) {
-
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-
-        if (error !== null) {
-            console.log('exec error: ' + error);
-        }
-
-    });
+    try {
+        shell.exec('./connection_profile.json')
+        res.apiResponse('success')
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 module.exports = { ping, createConnectionProfile }
