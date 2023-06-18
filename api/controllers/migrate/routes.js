9a9b6f98bfa784f0ca7223119018df825bd20b66
Reekaa <forgacsreka88@gmail.com>
2023-06-18 16:21:03 +0100

    add new route for destination profile

--- api/controllers/migrate/routes.js
+++ api/controllers/migrate/routes.js
@@ -1,7 +1,8 @@
 const routes = require('express').Router()
-const { ping, createConnectionProfile } = require('./handler')
+const { ping, createConnectionProfile, createDestinationConnectionProfile } = require('./handler')
 
 routes.get('/', ping)
 routes.get('/connection-profile', createConnectionProfile)
+routes.get('/destination-connection-profile', createDestinationConnectionProfile)
 
 module.exports = routes
