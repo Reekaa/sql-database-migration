996e428d2e3e4664bae8013cd2129230aee9c753
Reekaa <forgacsreka88@gmail.com>
2023-06-18 15:47:08 +0100

    update shell script

--- api/controllers/ping/connection_profile.sh
+++ api/controllers/ping/connection_profile.sh
@@ -1,5 +1,6 @@
-'curl --header \
- "Authorization: Bearer $(gcloud auth application-default print-access-token)" \ --header \
- "Content-Type: application/json" \ --data \
- @./connection_profile.json \ -X \
- POST \ https://datamigration.googleapis.com/v1/projects/db-migration-project-s2042203/locations/europe-west2/connectionProfiles?connectionProfileId=on-prem-sql-profile'
+#!/bin/bash
+
+curl --header \"Authorization: Bearer $(gcloud auth application-default print-access-token)" \ 
+     --header \ "Content-Type: application/json" \ 
+     --data \ @./connection_profile.json \ 
+     -X \ POST \ https://datamigration.googleapis.com/v1/projects/db-migration-project-s2042203/locations/europe-west2/connectionProfiles?connectionProfileId=on-prem-sql-profile
