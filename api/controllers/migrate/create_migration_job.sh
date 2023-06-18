ecf0cdf551d1633b1f3beade2240414922691989
Reekaa <forgacsreka88@gmail.com>
2023-06-18 19:03:44 +0100

    fix migratin job shell script

--- api/controllers/migrate/create_migration_job.sh
+++ api/controllers/migrate/create_migration_job.sh
@@ -2,6 +2,6 @@
 
 curl --header "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
      --header 'Content-Type: application/json' \
-     --data @/home/db_migration_project_s2042203/sql-database-migration/api/controllers/migrate/create_migration-job.json \
+     --data @/home/db_migration_project_s2042203/sql-database-migration/api/controllers/migrate/create_migration_job.json \
      -X POST \
      https://datamigration.googleapis.com/v1/projects/db-migration-project-s2042203/locations/europe-west2/migrationJobs?migrationJobId=sql-migrationjob&requestId=mysqlmigrationjob
