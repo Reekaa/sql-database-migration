374214feddf35d92a124d4a79a20675c54ff1b1b
Reekaa <forgacsreka88@gmail.com>
2023-06-18 18:36:05 +0100

    rename cloud sql profile

--- api/controllers/migrate/destination_connection_profile.sh
+++ api/controllers/migrate/destination_connection_profile.sh
@@ -4,4 +4,4 @@ curl --header "Authorization: Bearer $(gcloud auth application-default print-acc
      --header 'Content-Type: application/json' \
      --data @/home/db_migration_project_s2042203/sql-database-migration/api/controllers/migrate/destination_connection_profile.json \
      -X POST \
-     https://datamigration.googleapis.com/v1/projects/db-migration-project-s2042203/locations/europe-west2/connectionProfiles?connectionProfileId=target-sql-profile
+     https://datamigration.googleapis.com/v1/projects/db-migration-project-s2042203/locations/europe-west2/connectionProfiles?connectionProfileId=destination-sql-profile
